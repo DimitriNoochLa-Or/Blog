b057ab5f8c6bc9a3b42c0a0c423ac6e41e4a577c
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2014-10-31 12:08:38 -0700

    added comments

--- controller/create-db.php
+++ controller/create-db.php
@@ -7,7 +7,7 @@
 
 	if($connection->connect_error){
 		// if connection doesnt work the connection dies
-		die("Error: " . $connection->connect_error);
+		die("<p>Error: " . $connection->connect_error"<p>");
 	}
 	// selects data base and tells whether its exists or not
 	$exists = $connection->select_db($database);
@@ -19,29 +19,28 @@
 		//creates database blog_db
 		$query = $connection->query("CREATE DATABASE $database");
 		if($query){
-			echo "Successfully created a database: " . $database;
+			echo "<p>Successfully created a database: " . $database"<p>";
 			//outputs that query is true
 
 		}
 	}
 	//else statement is only executed if the database already exist
 	else {
-		echo "Database has already exists.";
+		echo "<p>Database has already exists.<p>";
 	}
-			$query = $connection->query("CREATE TABLE  posts ("
-				. "id int(11) NOT NULL AUTO_INCREMENT, " 
-				. "title varchar(255) NOT NULL,"
-				. "post text NOT NULL,"
-				. "PRIMARY KEY (id))");
-			//up to 11 values in integer
+	
+	$query = $connection->query("CREATE TABLE posts ("  //creates a table by query
 
-			if($query) {
-				echo "Successfully created table :posts";
-
-
-			}
-			else{
-				echo "$connection->error";
-			}
-
-	$connection->close();
+	. "id int(11) NOT NULL AUTO_INCREMENT,"				// creates unique ids for the server and cannot be empty and handles the ids for us 
+	. "title varchar(255) NOT NULL,"					// creates a collumn for a title with a max length of 255 characters and it cant be empty
+	. "post text NOT NULL,"								//creates a collumn for text that cannot be empty
+	. "PRIMARY KEY(id))");							// allows the tables to be connected and is shown that it is connected by an id
+	
+	if ($query) {									// checks to see if this statement exists
+		
+		echo "<p>Successfully created table: posts</p>";   // lets you know that it succesfully created table:posts
+	}
+	else{
+		echo "<p>$connection->error</p>";			//lets you know that table:posts already exists
+	}
+	$connection->close();							
