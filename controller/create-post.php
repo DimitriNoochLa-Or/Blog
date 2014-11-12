69a6881dcdb862c53fae4d5b6183d330bb70a946
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2014-11-12 12:04:39 -0800

    added require_once to get file database.php created a new connection for post

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,14 @@
 <?php
-//filler_input prevents hacks
+	require_once(__DIR__ . "/../model/database.php");
+	// require_once is using the database .php
+	// making a new connection for my sequel, now have access to variables host,username,password and database.
+	$connection = new mysqli($host, $username, $password, $database);
+    //filter_input prevents hacks
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);//sanitizing it to make sure its a string 
 	//tells that it is what is posted
 	$post= filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
 	echo "<p>Title: $title </p>";
 	echo "<p>Post: $post </p>";
+//closing connection 
+	$connection->close();
